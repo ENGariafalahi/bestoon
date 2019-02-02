53b1cf33e7dde2ad6ca343e66edbcf6938b7affc
Aria Fallahi <ariafalahe10@gmail.com>
2019-02-02 13:58:12 +0330

    add income

--- web/admin.py
+++ web/admin.py
@@ -2,9 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Expense
+from .models import Expense, Income
 
 
 # Register your models here.
 
 admin.site.register(Expense)
+admin.site.register(Income)
--- web/models.py
+++ web/models.py
@@ -12,3 +12,10 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User)
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
